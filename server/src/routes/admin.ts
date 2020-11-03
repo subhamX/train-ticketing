daea35e716108684a940f4f0684ab86a79d3e18d
subham <subham.iitrpr@gmail.com>
2020-11-03 22:15:57 +0530

    Add admin route to define new coaches

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -12,8 +12,8 @@ app.post('/trains/add/', verifyToken, verifyAdmin, async (req, res) => {
         if (result.error === undefined) {
             let { train_number, train_name, source, destination } = req.body;
             await db.query(`INSERT INTO trains(train_number, train_name, source, destination)
-            VALUES($1, $2, $3, $4)`,[train_number, train_name, source, destination]);
-            
+            VALUES($1, $2, $3, $4)`, [train_number, train_name, source, destination]);
+
             res.send({
                 error: false,
                 message: `Train ${train_name} with ${train_number} inserted successfully`
@@ -26,5 +26,28 @@ app.post('/trains/add/', verifyToken, verifyAdmin, async (req, res) => {
     }
 })
 
+app.post('/coaches/add/', verifyToken, verifyAdmin, async (req, res) => {
+    try {
+        let { name, description, composition } = req.body;
+        // TODO: Add validation
+        let coach = await db.query(`INSERT INTO coaches(name, description)
+            VALUES($1, $2)
+            RETURNING *`, [name, description]);
+
+        let compositionTableName=coach.rows[0]['composition_table'];
+        let seats=composition.length;
+        for(let i=0;i<seats;i++){
+            await db.query(`INSERT INTO ${compositionTableName}(berth_number, berth_type)
+            VALUES($1, $2)`, [composition[i]['berth_number'], composition[i]['berth_type']]);
+        }
+        res.send({
+            error: false,
+            message: coach.rows[0]
+        })
+
+    } catch (err) {
+        res.send({ error: true, message: err.message })
+    }
+})
 
 export default app;
